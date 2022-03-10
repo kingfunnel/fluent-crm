81c698478b4e6e4b00203477bdab8fbc619b2007
anpimartinez <kingfunnel.co@gmail.com>
2022-03-10 16:50:39 -0500

    FluentCRM 2.5.8

---
+++ app/Http/Policies/FunnelPolicy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FluentCrm\App\Http\Policies;
+
+use FluentCrm\Framework\Request\Request;
+
+/**
+ *  FunnelPolicy - REST API Permission Policy
+ *
+ * @package FluentCrm\App\Http
+ *
+ * @version 1.0.0
+ */
+
+class FunnelPolicy extends BasePolicy
+{
+    /**
+     * Check user permission for any method
+     * @param  \FluentCrm\Framework\Request\Request $request
+     * @return Boolean
+     */
+    public function verifyRequest(Request $request)
+    {
+        if($request->method() == 'GET') {
+            return $this->currentUserCan('fcrm_read_funnels');
+        }
+
+        return $this->currentUserCan('fcrm_write_funnels');
+    }
+}
